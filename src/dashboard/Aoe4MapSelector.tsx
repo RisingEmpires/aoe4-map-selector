92bbcd9e87c945083962ea56f046106a25fe884e
Coow <eirik_skarding@hotmail.no>
2023-06-09 17:57:40 +0200

    Add Amount of Maps + Dropdown for each one
    Yikes this is some baaaaad code

--- src/dashboard/Aoe4MapSelector.tsx
+++ src/dashboard/Aoe4MapSelector.tsx
@@ -1,7 +1,7 @@
-import { useFunc } from 'ajv/dist/compile/util';
 import React, { useEffect, useState } from 'react';
 import Select from "react-select";
 import { useReplicant } from 'use-nodecg';
+import * as NumericInput from "react-numeric-input";
 
 interface DropdownOption {
 	value: string;
@@ -11,29 +11,114 @@ interface DropdownOption {
 
 export function Aoe4MapSelector() {
 
-	const [map, set_map] = useReplicant<DropdownOption>('map', { value: '', label: '' });
+	//Images of all maps
 	const [maps, set_maps] = useReplicant<Array<any>>('assets:maps', []);
-	//@ts-ignore
 
+	//You know.. I'm starting to think me coding is not a good idea
+	const [map1, set_map1] = useReplicant<DropdownOption>('map1', { value: '', label: '' });
+	const [map2, set_map2] = useReplicant<DropdownOption>('map2', { value: '', label: '' });
+	const [map3, set_map3] = useReplicant<DropdownOption>('map3', { value: '', label: '' });
+	const [map4, set_map4] = useReplicant<DropdownOption>('map4', { value: '', label: '' });
+	const [map5, set_map5] = useReplicant<DropdownOption>('map5', { value: '', label: '' });
+	const [map6, set_map6] = useReplicant<DropdownOption>('map6', { value: '', label: '' });
+	const [map7, set_map7] = useReplicant<DropdownOption>('map7', { value: '', label: '' });
+	const [map8, set_map8] = useReplicant<DropdownOption>('map8', { value: '', label: '' });
+	const [map9, set_map9] = useReplicant<DropdownOption>('map9', { value: '', label: '' });
+
+	const [amountOfMaps, set_amountOfMaps] = useReplicant<number>('amountOfMaps', 1);
+
+	//@ts-ignore
 	const [options, set_options] = useState([]);
 
-	//Sometimes just fuck TypeScript.. I give up.. Ignore Errors and it still work 5head
+	const [mapSelections, set_mapSelections] = useState([])
+
+	//Set the options in the dropdown menu to avaliable maps from /assets/aoe4-map-selector/maps
 	useEffect(() => {
+		console.log(maps)
+		if (!maps) return;
 		maps.forEach((element, i) => {
+			//Sometimes just fuck TypeScript.. I give up.. Ignore Errors and it still work 5head
+			var name = element.name
+			name = name.replace(/_/g, ' ');
+			console.log(name)
 			//@ts-ignore
-			set_options(oldArray => [...oldArray, { value: element.url, label: element.name }]);
+			set_options(oldArray => [...oldArray, { value: element.url, label: name }]);
 		});
-		//set_options(_options)
 		console.log(JSON.stringify(options))
 	}, [maps]);
 
-	const handleChange = (selectedOption) => {
-		set_map(selectedOption)
+
+	//Set dropdown lists depending on how many maps
+	//TODO: Revisit this in the future, didn't work as expected
+	/*
+	useEffect(() => {
+		let _array = []
+		for (let i = 0; i < amountOfMaps; i++) {
+			//@ts-ignore
+			_array.push(<Select options={options} onChange={handleChange} value={map} placeholder={'Select Map'} />)
+		}
+		set_mapSelections(_array)
+	}, [amountOfMaps])*/
+
+	//There has to be a better way of doing this.. I'm just to stupid to figure it out
+	const handleChange1 = (selectedOption) => {set_map1(selectedOption)}
+	const handleChange2 = (selectedOption) => {set_map2(selectedOption)}
+	const handleChange3 = (selectedOption) => {set_map3(selectedOption)}
+	const handleChange4 = (selectedOption) => {set_map4(selectedOption)}
+	const handleChange5 = (selectedOption) => {set_map5(selectedOption)}
+	const handleChange6 = (selectedOption) => {set_map6(selectedOption)}
+	const handleChange7 = (selectedOption) => {set_map7(selectedOption)}
+	const handleChange8 = (selectedOption) => {set_map8(selectedOption)}
+	const handleChange9 = (selectedOption) => {set_map9(selectedOption)}
+
+	const handleAmountchange = (newValue) => {
+		set_amountOfMaps(newValue)
 	}
 
 	return (
 		<>
-			<Select options={options} onChange={handleChange} value={map} placeholder={'Select Map'} />
+			<NumericInput min={1} max={9} value={amountOfMaps} onChange={handleAmountchange} />
+			{amountOfMaps}
+
+			<br></br>
+
+			<label>Map 1</label>
+			<Select options={options} onChange={handleChange1} value={map1} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 2</label>
+			<Select options={options} onChange={handleChange2} value={map2} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 3</label>
+			<Select options={options} onChange={handleChange3} value={map3} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 4</label>
+			<Select options={options} onChange={handleChange4} value={map4} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 5</label>
+			<Select options={options} onChange={handleChange5} value={map5} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 6</label>
+			<Select options={options} onChange={handleChange6} value={map6} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 7</label>
+			<Select options={options} onChange={handleChange7} value={map7} placeholder={'Select Map'} />
+			<br></br>
+
+
+			<label>Map 8</label>
+			<Select options={options} onChange={handleChange8} value={map8} placeholder={'Select Map'} />
+			<br></br>
+
+			<label>Map 9</label>
+			<Select options={options} onChange={handleChange9} value={map9} placeholder={'Select Map'} />
+			<br></br>
 		</>
 	)
+
 }
--- src/graphics/Index.tsx
+++ src/graphics/Index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useReplicant } from 'use-nodecg';
 
 interface DropdownOption {
@@ -8,11 +8,41 @@ interface DropdownOption {
 
 
 export function Index() {
-	const [map, set_map] = useReplicant<DropdownOption>('map', { value: '', label: '' });
+	const [map1, set_map1] = useReplicant<DropdownOption>('map1', { value: '', label: '' });
+	const [map2, set_map2] = useReplicant<DropdownOption>('map2', { value: '', label: '' });
+	const [map3, set_map3] = useReplicant<DropdownOption>('map3', { value: '', label: '' });
+	const [map4, set_map4] = useReplicant<DropdownOption>('map4', { value: '', label: '' });
+	const [map5, set_map5] = useReplicant<DropdownOption>('map5', { value: '', label: '' });
+	const [map6, set_map6] = useReplicant<DropdownOption>('map6', { value: '', label: '' });
+	const [map7, set_map7] = useReplicant<DropdownOption>('map7', { value: '', label: '' });
+	const [map8, set_map8] = useReplicant<DropdownOption>('map8', { value: '', label: '' });
+	const [map9, set_map9] = useReplicant<DropdownOption>('map9', { value: '', label: '' });
+	const [amountOfMaps, set_amountOfMaps] = useReplicant<number>('amountOfMaps', 1);
+
+
+	//This should probably just be done in the Dashboard instead of 9 different replicants
+	const maps = [map1, map2, map3, map4, map5, map6, map7, map8, map9]
+
+
+	//@ts-ignore
+	const [images, set_images] = useState([]);
+
+	useEffect(() => {
+		let _array = []
+		for (let i = 0; i < amountOfMaps; i++) {
+			//@ts-ignore
+			_array.push(<img key={i} src={maps[i].value} style={{ width: '120px', padding: '0px 10px'}} />)
+		}
+		set_images(_array)
+	}, [amountOfMaps, map1, map2, map3, map4, map5, map6, map7, map8, map9])
 
 	return (
 		<>
-			<img src={map.value}/>
+			<div style={{
+				position:'absolute', top:'50%', left:'50%',
+				transform: 'translate(-50%, -50%)',
+				display: 'flex'
+			}}>{images}</div>
 		</>
 	);
 }
